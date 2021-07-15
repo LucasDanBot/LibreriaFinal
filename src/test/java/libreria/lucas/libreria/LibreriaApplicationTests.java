a3356029928dc85feed66c980e7e356aa6f4d2bc
LucasDanBot <72525638+LucasDanBot@users.noreply.github.com>
2021-07-15 17:55:02 -0300

    Create LibreriaApplicationTests.java

---
+++ src/test/java/libreria/lucas/libreria/LibreriaApplicationTests.java
@@ -0,0 +1,13 @@
+package libreria.lucas.libreria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibreriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
