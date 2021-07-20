ee2b6f249c0d6c880b34f0ffbc62bfedfa8e75e7
LucasDanBot <72525638+LucasDanBot@users.noreply.github.com>
2021-07-20 20:19:46 -0300

    Create FotoRepositorio.java

---
+++ src/main/java/libreria/lucas/libreria/repositorios/FotoRepositorio.java
@@ -0,0 +1,13 @@
+package libreria.lucas.libreria.repositorios;
+
+import libreria.lucas.libreria.entidades.Foto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FotoRepositorio extends JpaRepository<Foto, Integer>{
+    
+    
+    
+    
+    
+    
+}
