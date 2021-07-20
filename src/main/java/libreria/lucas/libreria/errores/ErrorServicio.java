cafa18b76b7d9e3ddbc0fce5916151ee9fbb7011
LucasDanBot <72525638+LucasDanBot@users.noreply.github.com>
2021-07-19 21:04:44 -0300

    Create ErrorServicio.java

---
+++ src/main/java/libreria/lucas/libreria/errores/ErrorServicio.java
@@ -0,0 +1,11 @@
+package libreria.lucas.libreria.errores;
+
+public class ErrorServicio extends Exception{
+    
+    public ErrorServicio(String msj) {
+        
+        super(msj);
+        
+    }
+     
+}
