21b790ae8f3c0ecace4dd01d2af040ee87541e7f
LucasDanBot <72525638+LucasDanBot@users.noreply.github.com>
2021-07-21 17:12:43 -0300

    Update ErrorServicio.java

--- src/main/java/libreria/lucas/libreria/errores/ErrorServicio.java
+++ src/main/java/libreria/lucas/libreria/errores/ErrorServicio.java
@@ -4,6 +4,7 @@ public class ErrorServicio extends Exception{
     
     public ErrorServicio(String msj) {
         
+        // Le pasamos a la super clase el msj...
         super(msj);
         
     }
