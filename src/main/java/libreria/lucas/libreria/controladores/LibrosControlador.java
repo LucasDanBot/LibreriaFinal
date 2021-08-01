3a3e358d3f406a3bedad2078767ba4780b7b5a28
LucasDanBot <72525638+LucasDanBot@users.noreply.github.com>
2021-08-01 17:38:09 -0300

    Update LibrosControlador.java

--- src/main/java/libreria/lucas/libreria/controladores/LibrosControlador.java
+++ src/main/java/libreria/lucas/libreria/controladores/LibrosControlador.java
@@ -2,7 +2,9 @@ package libreria.lucas.libreria.controladores;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public class LibrosControlador {
  
@@ -11,10 +13,24 @@ public class LibrosControlador {
     public class PortalControlador {
 
         @GetMapping("/libros")
-        public String libros() {
+        public String libros(){
+            return "libros.html";
+        }
+        
+        @PostMapping("/guardarLibro")
+        public String guardarAutor(@RequestParam String isbn, @RequestParam String titulo, @RequestParam String anio, @RequestParam String ejemplares, @RequestParam String prestados, @RequestParam String autor, @RequestParam String editorial){
+            System.out.println("ISBN: " + isbn);
+            System.out.println("Titulo: " + titulo);
+            System.out.println("Año: " + anio);
+            System.out.println("Ejemplares: " + ejemplares);
+            System.out.println("Prestados: " + prestados);
+            System.out.println("Autor: " + autor);
+            System.out.println("Editorial: " + editorial);
             return "libros.html";
         }
 
+        
+        
     }
 
 
