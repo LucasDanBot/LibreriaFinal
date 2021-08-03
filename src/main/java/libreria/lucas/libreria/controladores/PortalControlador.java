b629c8497caca85d83f6e4a0b00a6c2a2e6c7029
LucasDanBot <72525638+LucasDanBot@users.noreply.github.com>
2021-08-02 21:27:55 -0300

    Update PortalControlador.java

--- src/main/java/libreria/lucas/libreria/controladores/PortalControlador.java
+++ src/main/java/libreria/lucas/libreria/controladores/PortalControlador.java
@@ -5,11 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
-
 @Controller
 @RequestMapping("/")
 public class PortalControlador {
-    
     
     @GetMapping("/")
     public String index(){
