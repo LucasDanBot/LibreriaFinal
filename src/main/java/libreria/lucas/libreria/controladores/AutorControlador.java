7340d1eaee3ba29870395dc3746cac9edbe571e1
LucasDanBot <72525638+LucasDanBot@users.noreply.github.com>
2021-08-02 21:27:51 -0300

    Create AutorControlador.java

---
+++ src/main/java/libreria/lucas/libreria/controladores/AutorControlador.java
@@ -0,0 +1,19 @@
+package libreria.lucas.libreria.controladores;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
+public class AutorControlador {
+    
+    @GetMapping("/agregarautor")
+    public String agregarautor(){
+        return "agregarautor.html";
+    }
+    
+    
+    
+    
+}
