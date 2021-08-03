43ea5f1dac33fba28a9fe3a337b0e343177a91a2
LucasDanBot <72525638+LucasDanBot@users.noreply.github.com>
2021-08-02 21:27:54 -0300

    Update LibrosControlador.java

--- src/main/java/libreria/lucas/libreria/controladores/LibrosControlador.java
+++ src/main/java/libreria/lucas/libreria/controladores/LibrosControlador.java
@@ -6,32 +6,31 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@Controller
+@RequestMapping("/libros")
 public class LibrosControlador {
- 
-    @Controller
-    @RequestMapping("/libros")
-    public class PortalControlador {
 
-        @GetMapping("/libros")
-        public String libros(){
-            return "libros.html";
-        }
-        
-        @PostMapping("/guardarLibro")
-        public String guardarAutor(@RequestParam String isbn, @RequestParam String titulo, @RequestParam String anio, @RequestParam String ejemplares, @RequestParam String prestados, @RequestParam String autor, @RequestParam String editorial){
-            System.out.println("ISBN: " + isbn);
-            System.out.println("Titulo: " + titulo);
-            System.out.println("Año: " + anio);
-            System.out.println("Ejemplares: " + ejemplares);
-            System.out.println("Prestados: " + prestados);
-            System.out.println("Autor: " + autor);
-            System.out.println("Editorial: " + editorial);
-            return "libros.html";
-        }
-
-        
-        
+    @GetMapping("/agregarlibro")
+    public String agregarlibro() {
+        return "agregarlibro.html";
     }
-
+    
+    @GetMapping("/buscarporautor")
+    public String buscarporautor() {
+        return "buscarporautor.html";
+    }
+    
+    
+    @PostMapping("/guardarLibro")
+    public String guardarAutor(@RequestParam String isbn, @RequestParam String titulo, @RequestParam String anio, @RequestParam String ejemplares, @RequestParam String prestados, @RequestParam String autor, @RequestParam String editorial) {
+        System.out.println("ISBN: " + isbn);
+        System.out.println("Titulo: " + titulo);
+        System.out.println("Año: " + anio);
+        System.out.println("Ejemplares: " + ejemplares);
+        System.out.println("Prestados: " + prestados);
+        System.out.println("Autor: " + autor);
+        System.out.println("Editorial: " + editorial);
+        return "libros.html";
+    }
 
 }
