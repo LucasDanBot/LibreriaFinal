4f89fd37c61749a5850726cfbd39f70077940f66
LucasDanBot <72525638+LucasDanBot@users.noreply.github.com>
2021-08-03 23:00:47 -0300

    Update AutorServicio.java

--- src/main/java/libreria/lucas/libreria/servicios/AutorServicio.java
+++ src/main/java/libreria/lucas/libreria/servicios/AutorServicio.java
@@ -1,70 +1,73 @@
 package libreria.lucas.libreria.servicios;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import libreria.lucas.libreria.entidades.Autor;
 import libreria.lucas.libreria.errores.ErrorServicio;
 import libreria.lucas.libreria.repositorios.AutorRepositorio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-
 @Service
 public class AutorServicio {
-    
+
     @Autowired
     private AutorRepositorio autorRepositorio;
-    
-    public void registrarAutor (String nombre) throws ErrorServicio{
-        
+
+    public void registrarAutor(String nombre) throws ErrorServicio {
+
         validar(nombre);
-        
-        Autor autor = new Autor();
-        
-        autor.setNombre(nombre);
-        autor.setAlta(new Date());
-        
-        autorRepositorio.save(autor);
-    }
-    
-    
-    public void modificarAutor (String nombre, String nombreNuevo) throws ErrorServicio {
-        
-        validar(nombre);
-        
+
+        /*segunda validacion, si no existe ya un autor con ese nombre */
         Autor autor = autorRepositorio.buscarPorNombre(nombre);
-        
+
+        if (autor == null) {
+            Autor autor1 = new Autor();
+            autor1.setNombre(nombre);
+            autor1.setAlta(new Date());
+            autorRepositorio.save(autor1);
+        } else {
+            throw new ErrorServicio("Ya existe un autor con ese nombre");
+        }
+    }
+
+    public void modificarAutor(String nombre, String nombreNuevo) throws ErrorServicio {
+
+        validar(nombre);
+
+        Autor autor = autorRepositorio.buscarPorNombre(nombre);
+
         if (autor == null) {
             throw new ErrorServicio("No se encontro ningun Autor con ese nombre");
         }
-        
+
         if (nombreNuevo == null || nombreNuevo.isEmpty()) {
             throw new ErrorServicio("El nombre nuevo del Autor esta vacio");
         }
-        
+
         autor.setNombre(nombreNuevo);
-        
+
         autorRepositorio.save(autor);
-        
+
     }
-    
-    
-    private void BajaAutor (String nombre) throws ErrorServicio {
-        
+
+    private void BajaAutor(String nombre) throws ErrorServicio {
+
         validar(nombre);
-        
+
         Autor autor = autorRepositorio.buscarPorNombre(nombre);
-        
+
         if (autor == null) {
             throw new ErrorServicio("No se encontro ningun Autor con ese nombre");
         }
-        
+
         autor.setBaja(new Date());
-        
+
         autorRepositorio.save(autor);
-        
+
     }
- 
-    
+
     private void AltaAutor(String nombre) throws ErrorServicio {
 
         validar(nombre);
@@ -80,13 +83,13 @@ public class AutorServicio {
         autorRepositorio.save(autor);
 
     }
-    
-    private void validar (String nombre) throws ErrorServicio{
-        
+
+    private void validar(String nombre) throws ErrorServicio {
+
         if (nombre == null || nombre.isEmpty()) {
             throw new ErrorServicio("El nombre del Autor esta vacio");
         }
-        
+
     }
 
 }
