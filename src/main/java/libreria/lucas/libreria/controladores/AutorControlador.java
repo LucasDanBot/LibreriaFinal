c9443c07edc90f87b9c104aaef63315f1926a930
LucasDanBot <72525638+LucasDanBot@users.noreply.github.com>
2021-08-03 23:00:41 -0300

    Update AutorControlador.java

--- src/main/java/libreria/lucas/libreria/controladores/AutorControlador.java
+++ src/main/java/libreria/lucas/libreria/controladores/AutorControlador.java
@@ -1,16 +1,37 @@
 package libreria.lucas.libreria.controladores;
 
+import libreria.lucas.libreria.errores.ErrorServicio;
+import libreria.lucas.libreria.servicios.AutorServicio;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/autores")
 public class AutorControlador {
     
+    
+    @Autowired
+    private AutorServicio autorservicio;
+    
     @GetMapping("/agregarautor")
     public String agregarautor(){
         return "agregarautor.html";
+    }
+    
+    @PostMapping("/guardarautor")
+    public String guardarAutor(ModelMap modelo, String nombre) {
+        
+        try{
+            autorservicio.registrarAutor(nombre);
+        } catch (ErrorServicio er) {
+            modelo.put("error",er.getMessage());
+            return "agregarlibro.html";
+        }
+        return "agrgarautor.html";
     }
     
     
