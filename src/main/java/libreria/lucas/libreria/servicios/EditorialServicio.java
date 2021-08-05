d49e3a57c01b222eba0011dd7b9b59c9de39c4e3
LucasDanBot <72525638+LucasDanBot@users.noreply.github.com>
2021-08-04 21:24:40 -0300

    Update EditorialServicio.java

--- src/main/java/libreria/lucas/libreria/servicios/EditorialServicio.java
+++ src/main/java/libreria/lucas/libreria/servicios/EditorialServicio.java
@@ -1,9 +1,16 @@
 package libreria.lucas.libreria.servicios;
 
+import libreria.lucas.libreria.repositorios.EditorialRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 
 @Service
 public class EditorialServicio {
+    
+    @Autowired
+    private EditorialRepositorio editorialrespositorio;
+    
+    
     
 }
