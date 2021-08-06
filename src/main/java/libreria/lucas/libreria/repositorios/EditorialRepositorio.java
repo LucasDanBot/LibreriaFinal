5f4780a2eb62fe6e7593daa41fa691ce990dfd54
LucasDanBot <72525638+LucasDanBot@users.noreply.github.com>
2021-08-05 21:17:54 -0300

    Update EditorialRepositorio.java

--- src/main/java/libreria/lucas/libreria/repositorios/EditorialRepositorio.java
+++ src/main/java/libreria/lucas/libreria/repositorios/EditorialRepositorio.java
@@ -2,11 +2,14 @@ package libreria.lucas.libreria.repositorios;
 
 import libreria.lucas.libreria.entidades.Editorial;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EditorialRepositorio extends JpaRepository<Editorial, Integer>{
     
-    
+    @Query("SELECT c FROM Editorial c WHERE c.nombre = :nombre")
+    public Editorial buscarPorNombre (@Param("nombre") String nombre);
     
 }
