eac5794ba7f7deea63e74fe0e2ea1f7545c114da
LucasDanBot <72525638+LucasDanBot@users.noreply.github.com>
2021-08-12 21:55:32 -0300

    Update LibrosControlador.java

--- src/main/java/libreria/lucas/libreria/controladores/LibrosControlador.java
+++ src/main/java/libreria/lucas/libreria/controladores/LibrosControlador.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -75,5 +76,24 @@ public class LibrosControlador {
         
         return "listalibros.html";
     }
+    
+    @GetMapping("/listalibrosmodificar")
+    public String listarlibrosmodificar(ModelMap modelo){
+        
+        modelo.put("libros", libroservicio.listarlibros());
+        
+        return "listalibrosmodificar.html";
+    }
+    
+    @GetMapping("/modificarlibro/{isbn}")
+    public String modificarlibro(@PathVariable Integer isbn){
+        
+        System.out.println(isbn);
+        
+        
+        
+        return "modificarlibro.html";
+    }
+    
 
 }
