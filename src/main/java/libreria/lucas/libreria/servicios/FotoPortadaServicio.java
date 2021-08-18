a5ebd1f0ce9f84a968ac81547fe92b65bc9ba8b4
LucasDanBot <72525638+LucasDanBot@users.noreply.github.com>
2021-08-18 20:00:28 -0300

    Create FotoPortadaServicio.java

---
+++ src/main/java/libreria/lucas/libreria/servicios/FotoPortadaServicio.java
@@ -0,0 +1,39 @@
+package libreria.lucas.libreria.servicios;
+
+import javax.transaction.Transactional;
+import libreria.lucas.libreria.entidades.Foto;
+import libreria.lucas.libreria.errores.ErrorServicio;
+import libreria.lucas.libreria.repositorios.FotoPortadaRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class FotoPortadaServicio {
+
+    @Autowired
+    private FotoPortadaRepositorio fotoportadarepositorio;
+
+    @Transactional
+    public Foto guardar(MultipartFile archivo) throws ErrorServicio {
+
+        if (archivo != null) {
+
+            try {
+                Foto foto = new Foto();
+                foto.setMime(archivo.getContentType());
+                foto.setNombre(archivo.getName());
+                foto.setContenido(archivo.getBytes());
+
+                return fotoportadarepositorio.save(foto);
+
+            } catch (Exception e) {
+                throw new ErrorServicio("No se pudo leer el archivo que intenta cargar");
+            }
+        }
+        
+        return null;
+
+    }
+
+}
