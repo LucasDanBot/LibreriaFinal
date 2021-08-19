1e834a518440cd061b581ac327c36218cc087e90
LucasDanBot <72525638+LucasDanBot@users.noreply.github.com>
2021-08-19 19:47:48 -0300

    Update LibroServicio.java

--- src/main/java/libreria/lucas/libreria/servicios/LibroServicio.java
+++ src/main/java/libreria/lucas/libreria/servicios/LibroServicio.java
@@ -7,12 +7,13 @@ import java.util.Optional;
 import javax.transaction.Transactional;
 import libreria.lucas.libreria.entidades.Autor;
 import libreria.lucas.libreria.entidades.Editorial;
+import libreria.lucas.libreria.entidades.Foto;
 import libreria.lucas.libreria.entidades.Libro;
 import libreria.lucas.libreria.errores.ErrorServicio;
 import libreria.lucas.libreria.repositorios.LibroRepositorio;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class LibroServicio {
@@ -20,8 +21,11 @@ public class LibroServicio {
     @Autowired
     private LibroRepositorio libroRepositorio;
 
+    @Autowired
+    private FotoPortadaServicio fotoportadaservicio;
+
     @Transactional
-    public void registrarLibro(Long isbn, String titulo, Integer anio, Integer ejemplares, Integer prestados, Autor autor, Editorial editorial) throws ErrorServicio {
+    public void registrarLibro(MultipartFile fotoportada, Long isbn, String titulo, Integer anio, Integer ejemplares, Integer prestados, Autor autor, Editorial editorial) throws ErrorServicio {
 
         if (isbn == null) {
             throw new ErrorServicio("El ISBN no puede estar vacio");
@@ -58,6 +62,11 @@ public class LibroServicio {
             throw new ErrorServicio("Debe seleccionar un autor");
         }
 
+        Foto foto = new Foto();
+        if (fotoportada != null) {
+            foto = fotoportadaservicio.guardar(fotoportada);
+        }
+
         Libro libro = new Libro();
 
         libro.setIsbn(isbn);
@@ -68,6 +77,7 @@ public class LibroServicio {
         libro.setAlta(new Date());
         libro.setAutor(autor);
         libro.setEditorial(editorial);
+        libro.setFoto(foto);
 
         libroRepositorio.save(libro);
 
@@ -92,7 +102,7 @@ public class LibroServicio {
     }
 
     @Transactional
-    public void modificarLibro(Long isbn, String titulo, Integer anio, Integer ejemplares, Integer prestados, Autor autor, Editorial editorial) throws ErrorServicio {
+    public void modificarLibro(MultipartFile fotoportada, Long isbn, String titulo, Integer anio, Integer ejemplares, Integer prestados, Autor autor, Editorial editorial) throws ErrorServicio {
 
         if (isbn == null) {
             throw new ErrorServicio("El ISBN no puede estar vacio");
@@ -106,25 +116,34 @@ public class LibroServicio {
             if (titulo == null || titulo.isEmpty()) {
                 throw new ErrorServicio("El Titulo no puede estar vacio");
             }
-
             if (anio == null) {
                 throw new ErrorServicio("El anio no puede estar vacio");
             }
-
             if (ejemplares == null) {
                 throw new ErrorServicio("Tiene que ingresar cuanto ejemplares llegaron");
             }
-
             if (prestados == null || prestados > ejemplares) {
                 throw new ErrorServicio("El numero de ejemplares prestados no puede estar vacio o ser mayor que el numero de ejemplares");
             }
-
+            if (autor == null) {
+                throw new ErrorServicio("Debe seleccionar un autor");
+            }
             if (autor == null) {
                 throw new ErrorServicio("Debe seleccionar un autor");
             }
 
-            if (autor == null) {
-                throw new ErrorServicio("Debe seleccionar un autor");
+            Foto foto = new Foto();
+            /* si la foto portada es nula (el usuario no carga una foto nueva), la foto a guardar sera la foto que ya tenga guardada en el caso 
+            de que exista, y si no tiene guardada ninguna seguira sin tenerla. Caso contrario, si el usuario carga una foto nueva, esta se guardara
+            piasando la existente si existiera */
+            if (fotoportada != null) {
+                if (L.get().getFoto() != null) {
+                    foto = fotoportadaservicio.modificarfoto(fotoportada, L.get().getFoto().getId());
+                } else {
+                    foto = fotoportadaservicio.guardar(fotoportada);
+                }
+            } else {
+                foto = L.get().getFoto();
             }
 
             Libro libro = new Libro();
@@ -137,13 +156,14 @@ public class LibroServicio {
             libro.setAlta(new Date());
             libro.setAutor(autor);
             libro.setEditorial(editorial);
+            libro.setFoto(foto);
 
             libroRepositorio.save(libro);
-            
+
         } else {
-            
-            throw new ErrorServicio("El ISBN ingresado ya existe");
-            
+
+            throw new ErrorServicio("El ISBN ingresado no existe");
+
         }
     }
 
