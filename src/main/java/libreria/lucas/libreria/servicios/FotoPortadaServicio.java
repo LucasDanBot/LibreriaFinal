9833aca04c8a93b851b400353b5325d64608ada0
LucasDanBot <72525638+LucasDanBot@users.noreply.github.com>
2021-08-19 19:47:47 -0300

    Update FotoPortadaServicio.java

--- src/main/java/libreria/lucas/libreria/servicios/FotoPortadaServicio.java
+++ src/main/java/libreria/lucas/libreria/servicios/FotoPortadaServicio.java
@@ -35,5 +35,32 @@ public class FotoPortadaServicio {
         return null;
 
     }
+    
+    @Transactional
+    public Foto modificarfoto(MultipartFile archivo, Integer id) throws ErrorServicio {
+        
+        if (archivo != null) {
+
+            try {
+                Foto foto = new Foto();
+                
+                foto.setId(id);
+                
+                foto.setMime(archivo.getContentType());
+                foto.setNombre(archivo.getName());
+                foto.setContenido(archivo.getBytes());
+
+                return fotoportadarepositorio.save(foto);
+
+            } catch (Exception e) {
+                throw new ErrorServicio("No se pudo leer el archivo que intenta cargar");
+            }
+        }
+        
+        return null;
+        
+        
+        
+    }
 
 }
