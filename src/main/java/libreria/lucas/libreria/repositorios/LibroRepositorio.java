754a6daa4fc9c1989d6a15a523fe32e37fc38dae
LucasDanBot <72525638+LucasDanBot@users.noreply.github.com>
2021-08-21 19:27:23 -0300

    Update LibroRepositorio.java

--- src/main/java/libreria/lucas/libreria/repositorios/LibroRepositorio.java
+++ src/main/java/libreria/lucas/libreria/repositorios/LibroRepositorio.java
@@ -4,14 +4,21 @@ import java.util.List;
 import libreria.lucas.libreria.entidades.Libro;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LibroRepositorio extends JpaRepository<Libro, Long>{
     
-    /*
-    @Query("SELECT l FROM Libro l WHERE l.baja <=> :null")
-    public List<Libro> buscarPorNombre ();
-    */
+    @Query("SELECT l FROM Libro l WHERE l.autor.nombre = :nombre")
+    public List<Libro> buscarPorNombre (@Param("nombre") String nombre);
+    
+    @Query("SELECT l FROM Libro l WHERE l.baja IS NOT NULL")
+    public List<Libro> buscarPorNombreNoActivos ();
+    
+    @Query("SELECT l FROM Libro l WHERE l.baja IS NULL")
+    public List<Libro> buscarPorNombreActivos ();
+    
+    
     
 }
