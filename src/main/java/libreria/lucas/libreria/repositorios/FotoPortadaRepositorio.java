bddcb88d0deb2bcf08689c0b2fb662175ecf9e3a
LucasDanBot <72525638+LucasDanBot@users.noreply.github.com>
2021-08-23 21:04:17 -0300

    Update FotoPortadaRepositorio.java

--- src/main/java/libreria/lucas/libreria/repositorios/FotoPortadaRepositorio.java
+++ src/main/java/libreria/lucas/libreria/repositorios/FotoPortadaRepositorio.java
@@ -1,11 +1,11 @@
 package libreria.lucas.libreria.repositorios;
 
-import libreria.lucas.libreria.entidades.Foto;
+import libreria.lucas.libreria.entidades.FotoPortada;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface FotoPortadaRepositorio extends JpaRepository<Foto, Integer> {
+public interface FotoPortadaRepositorio extends JpaRepository<FotoPortada, Integer> {
     
     
     
