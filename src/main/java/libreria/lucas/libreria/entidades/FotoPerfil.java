4b2f496b7e5a0536fd6ca6dd508f9989225cc614
LucasDanBot <72525638+LucasDanBot@users.noreply.github.com>
2021-08-23 21:04:07 -0300

    Create FotoPerfil.java

---
+++ src/main/java/libreria/lucas/libreria/entidades/FotoPerfil.java
@@ -0,0 +1,75 @@
+package libreria.lucas.libreria.entidades;
+
+import javax.persistence.Basic;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+
+@Entity
+public class FotoPerfil {
+    
+    // PARAMETROS --------------------------------------------------------------
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Auto genera este valor 1,2,3.... n
+    private Integer id;
+    
+    private String nombre;
+    
+    // usamos este atributo para indicarle al navegador que tipo de archivo es
+    private String mime;
+    
+    @Lob @Basic(fetch = FetchType.LAZY)
+    private byte[] contenido;
+
+    
+    // CONSTRUCTORES -----------------------------------------------------------    
+
+    public FotoPerfil() {
+    }
+
+    public FotoPerfil(Integer id, String nombre, String mime, byte[] contenido) {
+        this.id = id;
+        this.nombre = nombre;
+        this.mime = mime;
+        this.contenido = contenido;
+    }
+    
+    
+    // GETTER Y SETTERS --------------------------------------------------------    
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getMime() {
+        return mime;
+    }
+
+    public void setMime(String mime) {
+        this.mime = mime;
+    }
+
+    public byte[] getContenido() {
+        return contenido;
+    }
+
+    public void setContenido(byte[] contenido) {
+        this.contenido = contenido;
+    }
+    
+}
