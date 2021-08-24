f1114877a238c764929d5ff0bd969df0b1fc516c
LucasDanBot <72525638+LucasDanBot@users.noreply.github.com>
2021-08-23 21:04:14 -0300

    Update Usuario.java

--- src/main/java/libreria/lucas/libreria/entidades/Usuario.java
+++ src/main/java/libreria/lucas/libreria/entidades/Usuario.java
@@ -22,7 +22,7 @@ public class Usuario {
     private String clave;
     
     @OneToOne
-    private Foto foto;
+    private FotoPerfil fotoperfil;
     
     @Temporal (TemporalType.TIMESTAMP)
     private Date alta;
@@ -37,9 +37,86 @@ public class Usuario {
     public Usuario() {
     }
 
-    
+    public Usuario(Integer dni, String nombre, String apellido, String email, String clave, FotoPerfil fotoperfil, Date alta, Date baja) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+        this.clave = clave;
+        this.fotoperfil = fotoperfil;
+        this.alta = alta;
+        this.baja = baja;
+    }
     
     // GETTER Y SETTERS --------------------------------------------------------
+
+    public Integer getDni() {
+        return dni;
+    }
+
+    public void setDni(Integer dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+    public FotoPerfil getFotoperfil() {
+        return fotoperfil;
+    }
+
+    public void setFotoperfil(FotoPerfil fotoperfil) {
+        this.fotoperfil = fotoperfil;
+    }
+
+    public Date getAlta() {
+        return alta;
+    }
+
+    public void setAlta(Date alta) {
+        this.alta = alta;
+    }
+
+    public Date getBaja() {
+        return baja;
+    }
+
+    public void setBaja(Date baja) {
+        this.baja = baja;
+    }
+
+    
+
+   
     
     
     
